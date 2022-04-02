3334f6b5d788f9e49e1800423e169df872ae68ea
EC2 Default User <ec2-user@ip-172-31-90-29.ec2.internal>
2022-04-02 12:47:31 +0000

    [add]kadai2-5修正

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,11 +3,12 @@ class SearchesController < ApplicationController
 
   def search
     @range = params[:range]
-
+    @search = params[:search]
+    @word = params[:word]
     if @range == "User"
-      @users = User.looks(params[:search], params[:word])
+      @users = User.looks(@search, @word)
     else
-      @books = Book.looks(params[:search], params[:word])
+      @books = Book.looks(@search, @word)
     end
   end
 end
