c0ea144458b1a2d27308fb0309349353c3f1935d
EC2 Default User <ec2-user@ip-172-31-90-29.ec2.internal>
2022-04-11 14:56:16 +0000

    [start]kadai2-8a

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :favorited_books, through: :favorites, source: :book
+  has_many :user_rooms
+  has_many :chats
   has_one_attached :profile_image
 
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,8 @@ Rails.application.routes.draw do
   end
 
   get "search" => "searches#search"
+
+  get 'chat/:id' => 'chats#show', as: 'chat'
+  resources :chats, only: [:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
